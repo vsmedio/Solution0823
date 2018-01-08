e15832bff90d11c8a5b34d905bb8d8c7db727056
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 16:45:15 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Омовение Рамы
+Написать программу, которая вводит с клавиатуры строку текста.
+Программа заменяет в тексте первые буквы всех слов на заглавные.
+Вывести результат на экран.
+
+Пример ввода:
+мама мыла раму.
+
+Пример вывода:
+Мама Мыла Раму.
+
+
+Требования:
+1. Программа должна выводить текст на экран.
+2. Программа должна считывать строку с клавиатуры.
+3. Класс Solution должен содержать один метод.
+4. Программа должна заменять в тексте первые буквы всех слов на заглавные.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+
+/*
+        StringBuilder builder = new StringBuilder();
+        for (String word : s.split(" ")) {
+            if (!word.isEmpty()) {
+                char[] chars = word.toCharArray();
+                chars[0] = Character.toUpperCase(chars[0]);
+                builder.append(chars).append(" ");
+            }
+        }
+        System.out.println(builder.deleteCharAt(builder.length() - 1).toString());
+*/
+
+        for (String word : s.split("\\b")) {
+            System.out.print(word.substring(0, 1).toUpperCase() + word.substring(1));
+        }
+    }
+}
